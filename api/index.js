c6e2b49495b5f7041dfc73831385da5eef7d89a7
mehmetpektass <pektasmehmett1184@gmail.com>
2024-07-26 13:02:33 +0300

    created api of sign up

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 
 dotenv.config();
@@ -23,3 +24,4 @@ app.listen(port, () => {
 
 
 app.use('/api/user' , userRouter)
+app.use('/api/auth' , authRouter)
