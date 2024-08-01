d98976b41b7af3132b0fe34702d9c435d445ca5e
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-01 09:45:10 +0300

    created error page and auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs'
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req,res) => {
+export const signUp = async (req,res,next) => {
     const { username , email , password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({massage :'All fields are required'})
+       next(errorHandler(400 , 'All Fields Are Required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password , 10)
@@ -22,7 +23,7 @@ export const signUp = async (req,res) => {
         await newUser.save();
         res.json('Sign Up Successful')
     } catch (error) {
-        res.status(500).json({message: error.massage})
+        next(error)
     }
 
    
