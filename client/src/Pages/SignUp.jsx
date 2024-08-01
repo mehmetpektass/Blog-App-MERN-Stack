a4de29bd9c2b1dec970dc6b6d93ab12e932f28a4
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-01 12:29:18 +0300

    added functionality to the sign up page

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,43 +1,99 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields!");
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-10">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         <div className="flex-1">
-          <Link
-            to="/"
-            className="font-semibold dark:text-white text-4xl"
-          >
+          <Link to="/" className="font-semibold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-blue-500 via-red-400 to-pink-300 rounded-lg text-white">
               Mehmet's
             </span>
             Blog
           </Link>
-          <p className="text-sm mt-5">"Welcome to Mehmet's Blog! Explore articles on technology, travel, and personal development. Enjoy your reading!"</p>
+          <p className="text-sm mt-5">
+            "Welcome to Mehmet's Blog! Explore articles on technology, travel,
+            and personal development. Enjoy your reading!"
+          </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-3">
+          <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
             <div className="">
-              <Label value="Your Username"/>
-              <TextInput type="text" placeholder="Username" id="username" />
+              <Label value="Your Username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
-              <Label value="Your Email"/>
-              <TextInput type="email" placeholder="name@company.com" id="email" />
+              <Label value="Your Email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
-              <Label value="Your Password"/>
-              <TextInput type="password" placeholder="Password" id="password" />
+              <Label value="Your Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToBlue' type="submit">Sign Up</Button>
+            
           </form>
           <div className="flex gap-2 text-sm mt-3">
             <span>Have an Account?</span>
-            <Link to='/sign-in' className="text-blue-400" >Sign In</Link>
+            <Link to="/sign-in" className="text-blue-400">
+              Sign In
+            </Link>
           </div>
+          {errorMessage && (
+            <Alert className="mt-3" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
