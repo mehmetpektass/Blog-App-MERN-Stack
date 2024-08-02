b37c346559c3bc7521f9efcc4e1f1d69f3687796
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-02 20:03:45 +0300

    created sign in page api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,30 +1,57 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
-export const signUp = async (req,res,next) => {
-    const { username , email , password} = req.body;
+export const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === '' || email === '' || password === ''){
-       next(errorHandler(400 , 'All Fields Are Required'));
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(errorHandler(400, "All Fields Are Required"));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("Sign Up Successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All Fields Are Required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
     }
-
-    const hashedPassword = bcryptjs.hashSync(password , 10)
-
-    const newUser = new User({
-        username,
-        email,
-        password:hashedPassword,
-    })
-
-    
-
-    try {
-        await newUser.save();
-        res.json('Sign Up Successful')
-    } catch (error) {
-        next(error)
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid Password"));
     }
 
    
-}
+  } catch (error) {
+    next(error);
+  }
+};
