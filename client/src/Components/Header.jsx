4edc3d51e01fbf328caa5a0f1195804ad13cda9e
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-08 15:14:03 +0300

    modified dark mode's functionality for moon and sun

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
       </Button>
       <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill onClick={() => disptch(toggleTheme())}>
-        <FaMoon/>
+          {theme === 'light' ? <FaMoon/> : <FiSun/>}
         </Button>
         {currentUser ? (
           <Dropdown
