ee0fdfdf4482c0251a2ec7ae464a5752d84d0d06
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-09 10:10:56 +0300

    started creating dashboard

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSidebar from "../Components/DashSidebar";
+import DashProfile from "../Components/DashProfile";
 
 const Dashboard = () => {
   const location = useLocation();
-  const [tab , setTab] = useState('');
+  const [tab, setTab] = useState("");
   useEffect(() => {
-    const urlParams = new URLSearchParams(location.search)
-    const tabFromUrl = urlParams.get('tab')
-    console.log(tabFromUrl);
-  },[location.search])
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
 
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="div">
+      <div className="div">
+        <DashSidebar />
+      </div>
+      {tab === 'profile' && <DashProfile/>}
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
