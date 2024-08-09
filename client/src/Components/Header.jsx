cc9278c06956383c13b8d1541755e406fe37cee3
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-09 10:02:16 +0300

    fixed an error and set the tab on dashboard

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -47,7 +47,7 @@ const Header = () => {
             }
           >
             <Dropdown.Header>
-              <span className="block text-sm" text >@{currentUser.username}</span>
+              <span className="block text-sm" >@{currentUser.username}</span>
               <span className="block text-sm font-medium truncate" >{currentUser.email}</span>
             </Dropdown.Header>
             <Link to='/dashboard?tab=profile'>
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Dashboard = () => {
+  const location = useLocation();
+  const [tab , setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    console.log(tabFromUrl);
+  },[location.search])
+
   return (
     <div>Dashboard</div>
   )
