7c75a269587c6f03d27d93dd322876760d6d1676
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-19 10:37:43 +0300

    completed signout api codes

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteUser, test, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, signout, test, updateUser } from '../controllers/user.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const userRouter = express.Router();
@@ -7,5 +7,6 @@ const userRouter = express.Router();
 userRouter.get('/test' ,test)
 userRouter.put('/update/:userId' , verifyUser , updateUser)
 userRouter.delete('/delete/:userId' , verifyUser , deleteUser)
+userRouter.post('/signout' , signout)
 
 export default userRouter;
