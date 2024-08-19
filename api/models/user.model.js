5ca52184c8c40741dcc28e049b348815b5879812
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-19 15:57:43 +0300

    added is admin part on user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://via.placeholder.com/150",
     },
+    isAdmin: {
+      type:Boolean,
+      default:false
+    }
   },
   { timestamps: true }
 );
