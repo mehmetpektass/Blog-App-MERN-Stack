d6d895626bab787b462334b387c9f66007d216cd
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-21 23:01:39 +0300

    added post tab when it's clicked

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import DashSidebar from "../Components/DashSidebar";
 import DashProfile from "../Components/DashProfile";
+import { DashPosts } from "../Components/DashPosts";
 
 const Dashboard = () => {
   const location = useLocation();
@@ -20,6 +21,7 @@ const Dashboard = () => {
         <DashSidebar />
       </div>
       {tab === 'profile' && <DashProfile/>}
+      {tab === 'posts' && <DashPosts/>}
     </div>
   );
 };
