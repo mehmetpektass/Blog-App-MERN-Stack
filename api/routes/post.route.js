dffb3145d851c62d6a8e84e0d423ce784d2f4e64
mehmetpektass <pektasmehmett1184@gmail.com>
2024-08-22 15:18:12 +0300

    created get some infos about posts api codes

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { create } from '../controllers/post.controller.js';
+import { create, getPost } from '../controllers/post.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const postRouter = express.Router();
 
 postRouter.post('/create' , verifyUser, create);
+postRouter.get('/getPost' , getPost)
 
 export default postRouter;
