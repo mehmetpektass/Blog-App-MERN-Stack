ab7447f7db1b77efcd58c99c63569bd8153b3ffe
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-03 15:20:11 +0300

    created post page and it's style

--- client/src/Pages/PostPage.jsx
+++ client/src/Pages/PostPage.jsx
@@ -1,5 +1,6 @@
+import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 const PostPage = () => {
   const { postSlug } = useParams();
   const [loading, setloading] = useState(false);
@@ -30,11 +31,43 @@ const PostPage = () => {
     fetchPost();
   }, [postSlug]);
 
-  return;
-  <div>
-    PostPage
-    {error && <p>{error}</p>}
-  </div>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Spinner size="xl" />
+      </div>
+    );
+
+  return (
+    <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
+      <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
+        {post && post.title}
+      </h1>
+      <Link
+        to={`/search?category=${post && post.category}`}
+        className='self-center mt-5'
+      >
+        <Button color='gray' pill size='xs'>
+          {post && post.category}
+        </Button>
+      </Link>
+      <img
+        src={post && post.image}
+        alt={post && post.title}
+        className='mt-10 p-3 max-h-[600px] w-full object-cover'
+      />
+      <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+        <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+        <span className='italic'>
+          {post && (post.content.length / 100).toFixed(0)} mins read
+        </span>
+      </div>
+      <div
+        className='p-3 max-w-2xl mx-auto w-full post-content'
+        dangerouslySetInnerHTML={{ __html: post && post.content }}
+      ></div>
+    </main>
+  );
 };
 
 export default PostPage;
