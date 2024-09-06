cd2ae1453f55acfb40f64bdabbab8307ffbd6e26
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-06 09:26:34 +0300

    created like function of api side

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createComment, getComments } from '../controllers/comment.controller.js';
+import { createComment, getComments, likeComment } from '../controllers/comment.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const commentRouter = express.Router();
 
 commentRouter.post('/create' , verifyUser ,createComment);
-commentRouter.get('/getpostcomments/:postId' , getComments)
+commentRouter.get('/getpostcomments/:postId' , getComments);
+commentRouter.post('/likeComment/:commentId', verifyUser , likeComment)
 
 export default commentRouter;
