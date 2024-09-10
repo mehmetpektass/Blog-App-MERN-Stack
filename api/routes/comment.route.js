ac47aa0972675e0499d3212b3d8fe20533214c8a
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-10 09:15:29 +0300

    created api codes for editing a comment of api

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { createComment, getComments, likeComment } from '../controllers/comment.controller.js';
+import { createComment, editComment, getComments, likeComment } from '../controllers/comment.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const commentRouter = express.Router();
 
 commentRouter.post('/create' , verifyUser ,createComment);
 commentRouter.get('/getpostcomments/:postId' , getComments);
-commentRouter.put('/likecomment/:commentId', verifyUser , likeComment)
+commentRouter.put('/likecomment/:commentId', verifyUser , likeComment);
+commentRouter.put('editcomment/:commentId' , verifyUser , editComment)
 
 export default commentRouter;
