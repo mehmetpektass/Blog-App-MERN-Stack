6e3cd5e08026798e462a8c0b23a482bc04f8c47e
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-10 21:40:53 +0300

    formated document

--- api/index.js
+++ api/index.js
@@ -1,45 +1,44 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
-import cookieParser from 'cookie-parser'
-import postRouter from './routes/post.route.js';
-import commentRouter from './routes/comment.route.js';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-mongoose.connect(
-  process.env.MONGO_URL
-).then( () => {
-    console.log('Database is connected')
-}).catch((err) => {
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(port, () => {
   console.log(`Server is Runing on Port ${port}`);
 });
 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/post", postRouter);
+app.use("/api/comment", commentRouter);
 
-app.use('/api/user' , userRouter)
-app.use('/api/auth' , authRouter)
-app.use('/api/post' , postRouter)
-app.use('/api/comment' , commentRouter)
-
-app.use((err , req , res , next) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || 'Internal Server Error'
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
-  })
-})
+  });
+});
