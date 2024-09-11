f2386b608b36d2134ed5a7645df2baca717533d5
mehmetpektass <pektasmehmett1184@gmail.com>
2024-09-11 11:32:29 +0300

    cretaed recent posts component

--- client/src/Components/CallToAction.jsx
+++ client/src/Components/CallToAction.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CallToAction = () => {
   return (
-    <div className='flex flex-col sm:flex-row p-3 border border-teal-300 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center max-w-3xl mx-auto'>
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-300 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center max-w-3xl mx-auto mb-8'>
     <div className="flex-1 justify-center flex flex-col">
         <h2 className='text-2xl'>
             Want to learn more about the projects?
